a01c4300a44c3f28dfc083fdbc8e4eeb1a65c0e9
brachi <40743558+brachiGH@users.noreply.github.com>
2022-08-28 22:54:14 +0000

    2

--- MLiteNotificationService.sh
+++ MLiteNotificationService.sh
@@ -2,9 +2,7 @@
 echo "MLite Notificaton Starting"
 
 MLitePushNotification() {
-	if : >/dev/tcp/8.8.8.8/53; then
-		am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.syncjob.MLitePushNotificationService
-	fi
+	am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.syncjob.MLitePushNotificationService
 }
 
 MLitePushNotification
