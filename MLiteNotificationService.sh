e91aa9f95a4cb6e46342ffedc73c1dbd64e96f83
brachi <40743558+brachiGH@users.noreply.github.com>
2022-08-29 10:56:47 +0000

    3

--- MLiteNotificationService.sh
+++ MLiteNotificationService.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 echo "MLite Notificaton Starting"
 
+UID=0 #Get MLite user ID or UID
+
+if [ $UID -eq 0	]; then
+	echo "Set MLite user ID or UID inside MLiteNotificationService.sh"
+fi
+
 MLitePushNotification() {
-	am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.syncjob.MLitePushNotificationService
+	isMLiteRunning=$(am get-uid-state $UID)
+
+	if [[ "$isMLiteRunning" == *"NONEXISTENT"* ]]; then
+		am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.syncjob.MLitePushNotificationService
+	fi
 }
 
 MLitePushNotification
