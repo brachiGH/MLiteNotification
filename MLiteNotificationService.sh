cde1b31525cf802810805b904b8c2c1a84dc9d8f
brachi <40743558+brachiGH@users.noreply.github.com>
2022-08-29 11:56:06 +0000

    4

--- MLiteNotificationService.sh
+++ MLiteNotificationService.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 echo "MLite Notificaton Starting"
 
-UID=0 #Get MLite user ID or UID
+UID=1 #Get MLite user ID or UID
 
 
 
 MLitePushNotification() {
-	isMLiteRunning=$(am get-uid-state $UID)
+        isMLiteRunning=$(am get-uid-state $UID)
 
-	if [[ "$isMLiteRunning" == *"NONEXISTENT"* ]]; then
-		am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.syncjob.MLitePushNotificationService
-	fi
+        echo $isMLiteRunning
+        if [[ "$isMLiteRunning" == *"NONEXISTENT"* ]]; then
+                am stop-service --user 0 -n com.facebook.mlite/com.facebook.mlite.notify.DelayedNotificationService
+                am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.notify.DelayedNotificationService
+                #am startforegroundservice --user 0 -n com.facebook.mlite/com.facebook.mlite.syncjob.MLitePushNotificationService
+        fi
 }
 
 
-if [ $UID -eq 0	]; then
-	echo "Set MLite user ID or UID inside MLiteNotificationService.sh"
+if [ $UID -eq 1 ]; then
+        echo "Set MLite user ID or UID inside MLiteNotificationService.sh"
 else
-	MLitePushNotification
-	while sleep 11; do MLitePushNotification; done
+        MLitePushNotification
+        while sleep 8; do MLitePushNotification; done
 fi
 
 #COMMAND TO RUN IS: su -c "sh ./MLitePushCommand.sh"
